69f246e9a5d080fca6cbe0fd23b48d7ee73fc222
JY <xirene@naver.com>
2023-05-19 21:12:35 +0900

    header profile switch components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
-
+import Profile from "./components/Profile";
+import Switch from "./components/Switch";
 function App() {
   return (
     <>
       <Header />
+      <Profile />
+      <Switch />
     </>
   );
 }
