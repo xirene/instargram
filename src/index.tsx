6a84bca3f54bd85e47b0c481d61f1163f9a1f1d6
JY <xirene@naver.com>
2023-05-19 19:55:25 +0900

    test text

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./reset.css";
+import "./common.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
