d51f98eede8242c45e364ca3c189702fe0221104
JY <xirene@naver.com>
2023-05-19 20:20:00 +0900

    component setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="App">test</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
