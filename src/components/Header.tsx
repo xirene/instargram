a19b795c74237639b930735664550960b50e6f6c
JY <xirene@naver.com>
2023-05-19 21:13:06 +0900

    header init

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,25 @@
 function Header() {
-  return <div></div>;
+  return (
+    <>
+      <div className="header-wrap">
+        <div className="header">
+          <div className="header__logo-wrap">
+            <h1>logo</h1>
+          </div>
+          <div className="header__search-wrap">
+            <input placeholder="search" />
+          </div>
+          <div className="header__rnb-wrap">
+            <ul className="header__rnb">
+              <li>1</li>
+              <li>2</li>
+              <li>3</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Header;
