6147eee13d4e29dee79fb7a98890e3cb8a978e95
JY <xirene@naver.com>
2023-05-22 22:26:15 +0900

    add: useState useEffect random data 66 add infinity scroll data load + 9

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,8 +1,49 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import { useState, useEffect } from "react";
 
 function Post() {
-  const items = [1, 2, 3, 4, 5, 6, 7];
+  const data = Array.from({ length: 66 }, (_, index) => index + 1);
+  const startIndex = 0; // 시작 인덱스
+  let [endIndex, setEndIndex] = useState(8); // 끝 인덱스
+  const [fetching, setFetching] = useState(false);
+  let [items, setItems] = useState(
+    data.filter((_, index) => index >= startIndex && index <= endIndex)
+  );
+  function fetchMoreInstaFeeds() {
+    if (data.length > endIndex) {
+      setEndIndex(endIndex + 9);
+      setFetching(true);
+      setItems(
+        data.filter((_, index) => index >= startIndex && index <= endIndex)
+      );
+      setTimeout(() => setFetching(false), 1000);
+    } else {
+      setEndIndex(data.length);
+      setItems(
+        data.filter((_, index) => index >= startIndex && index <= endIndex)
+      );
+      setFetching(true);
+    }
+  }
+  const handleScroll = () => {
+    const scrollHeight = document.documentElement.scrollHeight;
+    const scrollTop = document.documentElement.scrollTop;
+    const clientHeight = document.documentElement.clientHeight;
+    if (scrollTop + clientHeight >= scrollHeight && fetching === false) {
+      // 페이지 끝에 도달하면 추가 데이터를 받아온다
+      fetchMoreInstaFeeds();
+    }
+  };
+
+  useEffect(() => {
+    // scroll event listener 등록
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      // scroll event listener 해제
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
   return (
     <>
       <div
